458db443832309b96e69dac9dee98cdc02aa38e7
Javier Antonio Hernandez De Paz <javiercito53@gmail.com>
2019-02-12 12:01:13 -0600

    frontend first section

---
+++ frontend/3de3/src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="app">
+    <!--header class="page-navbar sticky-top container"-->
+    <header class="page-navbar container">
+      <div class="row page-menu">
+          <div class="col-2">
+              <a id="navbar-brand" href="#inicio"><img class="img-fluid" src="./assets/3de3.jpg" alt="3de3 Guatemala" /></a>
+          </div>
+          <div class="col-10 page-menu-block">
+            <nav>
+              <ul class="navbar-nav menu bd-navbar-nav flex-row smooth-scroll justify-content-center">
+                <li class="nav-item"><router-link to="#aspirantes" class="nav-link">Aspirantes</router-link></li>
+                <li class="nav-item"><span class="nav-link">|</span></li>
+                <li class="nav-item"><router-link to="#nosotros" class="nav-link">Exige 3de3</router-link></li>
+                <li class="nav-item"><span class="nav-link">|</span></li>
+                <li class="nav-item"><router-link to="#transparencia" class="nav-link">Estadisitcas</router-link></li>
+                <li class="nav-item"><span class="nav-link">|</span></li>
+                <li class="nav-item"><router-link to="#contacto" class="nav-link">Publica tu 3de3</router-link></li>
+              </ul>
+            </nav>
+          </div>
+      </div>
+    </header>
+    
+    <!--div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div-->
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+li.nav-item {
+  padding: 5px 25px;
+  background: #EF2466;
+  color: #FFF;
+}
+
+li.nav-item a {
+  color: #FFF;
+}
+
+li.nav-item a:hover {
+  color: #0096DB;
+}
+
+.page-navbar{
+  padding-top:10px;
+}
+</style>
---
+++ frontend/3de3/src/views/Home.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="home">
+    <div class="container">
+      <div class="row">
+        <div class="col-2"></div>
+        <div class="col-10">
+          <img class="img-fluid" alt="3 de 3 Guatemala" src="../assets/title.jpg">
+        </div>
+      </div>
+      <div class="row text">
+        <div class="col-2"></div>
+        <div class="col-10">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut consequat scelerisque laoreet. Nunc ut mattis magna. Sed gravida nisi lacinia bibendum aliquet. Fusce feugiat felis ut sem porta luctus. In et leo a dui pretium sollicitudin nec vitae nunc. Praesent interdum ipsum ac luctus egestas. Aenean nec neque vitae mauris maximus posuere. Quisque scelerisque quam eu mi ornare, ac semper tellus vestibulum. Proin dapibus vehicula dui. In ornare quam mauris, id tristique arcu semper a. Nunc eget lacus quis orci cursus rhoncus vitae at elit. Aliquam pharetra massa eu orci volutpat, quis iaculis tellus tempus. Integer eu neque massa. Suspendisse arcu libero, pharetra vel dolor nec, sollicitudin accumsan nisl. Ut risus ligula, iaculis sit amet leo quis, placerat interdum augue. Cras iaculis ante sed suscipit posuere.</p>
+          <p>Nunc in lorem luctus metus auctor tempor at id arcu. Etiam eu turpis ac odio euismod convallis sit amet vitae nisl. Suspendisse sed rutrum augue, ut aliquam neque. Donec volutpat consequat metus, eu pharetra ex sagittis in. Donec auctor ipsum sit amet luctus mattis. Suspendisse eu neque quis nunc accumsan dictum. Praesent non tellus vitae nibh hendrerit iaculis. Nulla facilisi. Proin faucibus, felis id consequat convallis, lacus elit rutrum quam, et volutpat turpis arcu aliquam nunc. Vestibulum sagittis leo a ante sodales, eu consequat urna imperdiet. Morbi semper dictum ipsum, quis iaculis tortor varius ut. Nulla cursus, orci cursus ornare mollis, lorem elit efficitur leo, quis bibendum erat lacus id mi. Maecenas malesuada augue sit amet erat interdum, at viverra nulla rutrum. Suspendisse et hendrerit eros. Sed molestie consequat vulputate.</p>
+        </div>
+      </div>
+      <div class="row icons">
+        <div class="col-3"></div>
+        <div class="col-1">
+          <img class="img-fluid" alt="Solvencia Fiscal" src="../assets/sf.jpg" v-b-tooltip.hover title="La solvencia fiscal es ...">
+          <span>Solvencia Fiscal</span>
+        </div>
+        <div class="col-1"></div>
+        <div class="col-1">
+          <img class="img-fluid" alt="Declaración de Intereses" src="../assets/di.jpg" v-b-tooltip.hover title="La Declaración de intereses es ...">
+          <span>Declaración de Intereses</span>
+        </div>
+        <div class="col-1"></div>
+        <div class="col-1">
+          <img class="img-fluid" alt="Declaración Patrimonial" src="../assets/dp.jpg" v-b-tooltip.hover title="La Declaración Patrimonial es ...">
+          <span>Declaración Patrimonial</span>
+        </div>
+        <div class="col-1"></div>
+        <div class="col-3"></div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <HelloWorld msg="Welcome to Your Vue.js App"/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+
+</script>
+
+<style>
+.text {
+  text-align: justify;
+}
+.text p {
+  margin-top:35px;
+}
+.icons {
+  margin-top:35px;
+}
+.tooltip-inner {
+  color: #fff;
+  background-color: #EF2466;
+  border-radius: .25rem;
+}
+
+/*tooltip arrows*/
+.tooltip.bs-tooltip-top .arrow::before {
+  border-top-color: #EF2466 !important;
+}
+
+.tooltip.bs-tooltip-right .arrow::before {
+  border-right-color: #EF2466 !important;
+}
+
+.tooltip.bs-tooltip-bottom .arrow::before {
+  border-bottom-color: #EF2466 !important;
+}
+
+.tooltip.bs-tooltip-left .arrow::before {
+  border-left-color: #EF2466 !important;
+}
+
+
+</style>
