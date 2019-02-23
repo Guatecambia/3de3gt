a31bafbd5de385b3c57bd5cc5558959cb8d7945c
Javier Antonio Hernandez De Paz <javiercito53@gmail.com>
2019-02-22 18:15:07 -0600

    basic faq functionality, without left arrow in the quesiton

--- frontend/3de3/src/views/Faq.vue
+++ frontend/3de3/src/views/Faq.vue
@@ -6,71 +6,59 @@
       </div>
     </b-row>
     <b-row>
-      <div class="col-12">
-
-
-
-
-        <div id="accordion">
-          <b-card>
-            <b-card-header  id="headingOne">
-              <h5 class="mb-0">
-                <b-button v-b-toggle.collapseOne aria-expanded="true">
-                  Collapsible Group Item #1
-                </b-button>
-              </h5>
-            </b-card-header>
-
-            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-              <b-card-body>
-                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-              </b-card-body>
-            </div>
-          </b-card>
-          <b-card>
-            <b-card-header  id="headingTwo">
-              <h5 class="mb-0">
-                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                  Collapsible Group Item #2
-                </button>
-              </h5>
-            </b-card-header>
-            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-              <b-card-body>
-                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-              </b-card-body>
-            </div>
-          </b-card>
-          <b-card>
-            <b-card-header  id="headingThree">
-              <h5 class="mb-0">
-                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                  Collapsible Group Item #3
-                </button>
-              </h5>
-            </b-card-header>
-            <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-              <b-card-body>
-                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-              </b-card-body>
-            </div>
-          </b-card>
+      <div v-for="(question, index) in interrogation" class="col-12">
+        <div class="faq-question">
+          <b-button class="question-btn" v-b-toggle="'quest'+index">
+            {{ question.question }}
+          </b-button>
+          <b-collapse :id="'quest'+index" class="question-body">
+            {{ question.answer }}
+          </b-collapse>
         </div>
-
-
-
-
-
       </div>
     </b-row>
   </b-container>
 </template>
-
+<script>
+export default {
+  name: 'Applicant',
+  data() {
+    return {
+      interrogation: [
+        {
+          question: "¿Qué es una declaración patrimonial y por qué consideramos que es importante hacerla pública?",
+          answer: "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS."
+        },
+        {
+          question: "¿Qué es una declaración patrimonial y por qué consideramos que es importante hacerla pública?",
+          answer: "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS."
+        }
+      ]
+    }
+  }
+}
+</script>
 <style>
 p {
   text-align: justify;
 }
 h1 {
   text-align:center;
+}
+.faq-question {
+  text-align:justify;
+  margin-top:20px;
+}
+.question-btn, .btn, .btn-secondary, .btn:hover, .btn-secondary:hover {
+  background:none;
+  color: #333;
+  border:none;
+}
+.question-btn:focus, .question-btn:active, .btn-secondary:focus, .btn-secondary:active {
+   outline: none !important;
+   box-shadow: none;
+}
+.question-body {
+  padding-left: 30px;
 }
 </style>
