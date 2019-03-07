138480f1310ff8545ee0610dccac3fcd07f46074
Javier Antonio Hernandez De Paz <javiercito53@gmail.com>
2019-03-07 00:46:36 -0600

    importing all from secrets and adding gunicorn

--- backend/gt3de3/settings.py
+++ backend/gt3de3/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-from .settings_secret import DATABASES, DEBUG, SECRET_KEY
+from .settings_secret import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
